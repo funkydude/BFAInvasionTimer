99d104e6f24cd70c6092d7a4182e60ce8b4d71bc
funkydude <funkydude@users.noreply.github.com>
2019-02-14 23:19:00 +0000

    Locale tweak

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -4,7 +4,7 @@ local _, mod = ...
 local L = mod.L
 
 L.firstRunWarning = "Timer werden erst angezeigt, sobald du die erste Invasion siehst."
-L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s wird angegriffen!"
+L.underAttack = "|T%d:15:15:0:0:64:64:4:60:4:60|t %s wird angegriffen!"
 L.nextInvasions = "Nächsten Invasionen"
 L.next = "Nächste: %s"
 L.waiting = "Warten"
--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -4,7 +4,7 @@ local _, mod = ...
 local L = mod.L
 
 L.firstRunWarning = "타이머는 첫번째 침공을 확인하기 전까지 표시되지 않습니다."
-L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s|1이;가; 공격받고 있습니다!"
+L.underAttack = "|T%d:15:15:0:0:64:64:4:60:4:60|t %s|1이;가; 공격받고 있습니다!"
 L.nextInvasions = "다음 침공"
 L.next = "다음: %s"
 L.waiting = "대기 중"
