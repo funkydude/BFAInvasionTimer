bff809aae54dbd30e9329b57ea05cf05e1967b15
funkydude <funkydude@users.noreply.github.com>
2019-03-13 03:47:06 +0000

    Fix itIT file

--- Locales/itIT.lua
+++ Locales/itIT.lua
@@ -3,8 +3,8 @@ if GetLocale() ~= "itIT" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "I timer non verranno visualizzati fino a quando non vedrai la tua prima invasione."
---L.underAttack = "|T%d:15:15:0:0:64:64:4:60:4:60|t %s è sotto attacco!"
---L.nextInvasions = "Prossime invasioni"
---L.next = "Prossima: %s"
---L.waiting = "In attesa"
+L.firstRunWarning = "I timer non verranno visualizzati fino a quando non vedrai la tua prima invasione."
+L.underAttack = "|T%d:15:15:0:0:64:64:4:60:4:60|t %s è sotto attacco!"
+L.nextInvasions = "Prossime invasioni"
+L.next = "Prossima: %s"
+L.waiting = "In attesa"
